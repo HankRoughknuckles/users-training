c40cbd15327f0b1e1e66291383b26ce86e2c1b5c
Thomas Morris <tmorris@salsitasoft.com>
2021-08-10 15:46:39 +0200

    update the migration file

--- src/migrations/1624525074594_create-users-table.js
+++ src/migrations/1624525074594_create-users-table.js
@@ -15,11 +15,12 @@ exports.up = pgm => {
     INSERT INTO users ("firstName", "lastName", "jobTitle", "age")
     VALUES ('Daenerys', 'Targaryen', 'Mother of Dragons', 18),
            ('Jaime', 'Lannister', 'Kingslayer', 30),
-           ('Arya', 'Stark', 'No One', 15),
-           ('Peter', 'Baelish', 'Lord', 45),
+           ('Arya', 'Stark', 'Faceless Man', 15),
+           ('Peter', 'Baelish', 'Lord', 40),
            ('Ned', 'Stark', 'Lord', 47),
-           ('Ygritte', 'Firehair', 'Wildling', 20),
            ('Jon', 'Snow', 'Bastard', 18),
+           ('Mance', 'Rayder', 'King Beyond the Wall', 45),
+           ('Tyrion', 'Lannister', 'Lord', 25),
            ('Tormund', 'Giantsbane', 'Wildling', 36)
   `)
 };
